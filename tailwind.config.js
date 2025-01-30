5e2d0301c05cdb3d07a0301a9eefea4478e440ff
ahmedjoseph07 <ahmedjoseph11@gmail.com>
2025-01-31 02:31:24 +0600

    hero section designed

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                'custom-white': '#FFFFFF',
+                'custom-light': '#EFE3E3',
+            },
+            backgroundImage: {
+                'custom-gradient': 'linear-gradient(to bottom, #FFFFFF, #EFE3E3)', // Custom Gradient
+            },
+        },
+    },
+}
